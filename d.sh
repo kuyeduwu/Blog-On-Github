fa55b24c39d2e7a36dd0d1b39ca3d3827e62f582
rick <myslsh_2015@163.com>
2016-12-25 20:05:55 +0800

    Create the ubuntu preparations post

--- d.sh
+++ d.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
-#Pull the most current files from GitHub first
+#Push the new posts to GitHub
+git push -u origin master
+
+#Pull the most current files from GitHub
 git pull origin master
 
 #Gnerate and deploy to GitHub page

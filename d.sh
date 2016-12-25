285f20699b76d80f519d46ec461b281e220ef4a3
rick <myslsh_2015@163.com>
2016-12-25 21:57:28 +0800

    Change the deployer to ssh.

--- d.sh
+++ d.sh
@@ -3,9 +3,6 @@
 #Push the new posts to GitHub
 git push -u origin master
 
-#Pull the most current files from GitHub
-git pull origin master
-
 #Gnerate and deploy to GitHub page
 hexo generate
 hexo deploy
